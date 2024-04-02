457f664bc9cf518b086cdf6ee27daf2233bca29b
carlowxx <cardimi.maria2016@outlook.com>
2024-04-02 19:58:40 -0300

    update

---
+++ _footer.php
@@ -0,0 +1,22 @@
+</main>
+        <footer>
+
+            <a href="index.php">
+                <i class="fa-solid fa-house fa-fw"></i>
+            </a>
+            <div>
+                <div>&copy; 2024 Eu Mesmo!</div>
+                <a href="privacy.php">Políticas de Privacidade</a>
+            </div>
+            <a href="#wrap">
+                <i class="fa-solid fa-circle-up fa-fw"></i>
+            </a>
+
+        </footer>
+    </div>
+    <script src="assets/js/global.js"></script>
+</body>
+
+
+
+</html>
