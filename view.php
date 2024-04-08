2218e48b14f605607a7c418dfd0c9b373b44dbf9
carlowxx <cardimi.maria2016@outlook.com>
2024-04-08 16:45:02 -0300

    Create View

---
+++ view.php
@@ -0,0 +1,17 @@
+<?php         
+require ("_global.php") ; 
+$page = [
+    
+
+    "title" => "Início",
+
+    "css" => "index.css",
+    "js" => "index.js"
+];
+
+require("_header.php");
+
+?>
+<article></article>
+
+<?php require("_footer.php"); ?>
